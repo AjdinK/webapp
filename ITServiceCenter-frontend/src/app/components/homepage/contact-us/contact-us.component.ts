a6c765b1d4331ddf29ddb3797a42f5d779236a1f
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-04-24 17:43:10 +0100

    update

--- ITServiceCenter-frontend/src/app/components/homepage/contact-us/contact-us.component.ts
+++ ITServiceCenter-frontend/src/app/components/homepage/contact-us/contact-us.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {CommonModule, NgOptimizedImage} from "@angular/common";
 import {FormsModule} from "@angular/forms";
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact-us',
@@ -15,14 +16,34 @@ import {FormsModule} from "@angular/forms";
 })
 export class ContactUsComponent {
   constructor( ) {}
+
   JelPoslano : boolean = false;
+
   porukaKontakt:any = {
     ime:'',
     email:'',
     poruka:'',
   }
 
-  posalji(porukaKontakt:any) {
+  async posalji(porukaKontakt:any) {
   this.JelPoslano = true;
+  emailjs.init("rs86oYSzchfvEtq82");
+    let response = await emailjs.send("service_jqhy61m","template_o0gkayw",{
+    from_name: porukaKontakt.ime,
+    to_name: "Ajdin.kuduzovic@gmail.com",//itservicercenter@gmail.com
+    from_user: porukaKontakt.email,
+    message: porukaKontakt.message,
+    reply_to: "test",
+  });
+    alert("Vasa Poruka poslata")
+    this.clearForm();
+}
+
+  private clearForm() {
+      this.porukaKontakt.ime="";
+      this.porukaKontakt.email="";
+      this.porukaKontakt.poruka="";
+      this.JelPoslano = false;
   }
+  
 }
