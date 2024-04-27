e4c990a40a1dcd73c28026000f2514631987604e
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-04-27 12:54:37 +0100

    update

--- ITServiceCenter-frontend/src/app/components/prodavac/prodavac.component.ts
+++ ITServiceCenter-frontend/src/app/components/prodavac/prodavac.component.ts
@@ -1,29 +1,35 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   ProdavacGetAllEndpoint,
-  ProdavacGetAllResponseProdavac
-} from "../../endpoints/prodavac-endpoints/prodavac-get-all-endpoint";
-import {ProdavacSnimiEndpoint, ProdavacSnimiRequest} from "../../endpoints/prodavac-endpoints/prodavac-snimi-endpoint";
-import {GradGetAllEndpoint, GradGetAllResponseGrad} from "../../endpoints/grad-endpoints/grad-get-all-endpoint";
-import {FormsModule, NgForm, ReactiveFormsModule} from "@angular/forms";
-import {ProdavacBrisiEndpoint} from "../../endpoints/prodavac-endpoints/prodavac-brisi-endpoint";
-import {ServiserSnimiRequest} from "../../endpoints/serviser-endpoints/serviser-snimi-endpoint";
-import {ServiserGetAllResponseServiseri} from "../../endpoints/serviser-endpoints/serviser-get-all-endpoint";
+  ProdavacGetAllResponseProdavac,
+} from '../../endpoints/prodavac-endpoints/prodavac-get-all-endpoint';
+import {
+  ProdavacSnimiEndpoint,
+  ProdavacSnimiRequest,
+} from '../../endpoints/prodavac-endpoints/prodavac-snimi-endpoint';
+import {
+  GradGetAllEndpoint,
+  GradGetAllResponseGrad,
+} from '../../endpoints/grad-endpoints/grad-get-all-endpoint';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { ProdavacBrisiEndpoint } from '../../endpoints/prodavac-endpoints/prodavac-brisi-endpoint';
+import { ServiserSnimiRequest } from '../../endpoints/serviser-endpoints/serviser-snimi-endpoint';
+import { ServiserGetAllResponseServiseri } from '../../endpoints/serviser-endpoints/serviser-get-all-endpoint';
 
 @Component({
   selector: 'app-prodavac',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './prodavac.component.html',
-  styleUrl: './prodavac.component.css'
+  styleUrl: './prodavac.component.css',
 })
-export class ProdavacComponent implements OnInit{
+export class ProdavacComponent implements OnInit {
   constructor(
     private prodavacGetAllEndpoint: ProdavacGetAllEndpoint,
     private prodavacSnimiEndpoint: ProdavacSnimiEndpoint,
-    private prodavacBrisiEndpoint : ProdavacBrisiEndpoint,
-    private gradGetAllEndpoint: GradGetAllEndpoint,
+    private prodavacBrisiEndpoint: ProdavacBrisiEndpoint,
+    private gradGetAllEndpoint: GradGetAllEndpoint
   ) {}
 
   ngOnInit(): void {
@@ -36,24 +42,24 @@ export class ProdavacComponent implements OnInit{
   prodavacPodaci: ProdavacGetAllResponseProdavac[] | null = [];
   gradPodaci: GradGetAllResponseGrad[] | null = null;
 
-
   showProdavacTable: boolean = true;
   showProdavacEdit: boolean = false;
   showProdavacForm: boolean = false;
   searchProdavac: string = '';
 
-  formTitle:string = '';
+  formTitle: string = '';
 
   odabraniProdavac: ProdavacSnimiRequest | null = null;
-  noviProdavac : ProdavacSnimiRequest | null = null;
-
+  noviProdavac: ProdavacSnimiRequest | null = null;
 
   fetchGrad() {
     this.gradGetAllEndpoint.obradi().subscribe({
       next: (x) => {
         this.gradPodaci = x.gradovi;
       },
-      error: (x) => { alert("greska fetchGrad -> " + x.error) },
+      error: (x) => {
+        alert('greska fetchGrad -> ' + x.error);
+      },
     });
   }
 
@@ -62,9 +68,12 @@ export class ProdavacComponent implements OnInit{
       next: (x) => {
         this.prodavacPodaci = x.listaProdavac;
       },
-      error: (x) => { alert("greska fetchProdavac -> " + x.error) },
+      error: (x) => {
+        alert('greska fetchProdavac -> ' + x.error);
+      },
     });
   }
+
   filtrirajProdavac() {
     if (this.prodavacPodaci == null) return [];
     return this.prodavacPodaci.filter(
@@ -75,8 +84,8 @@ export class ProdavacComponent implements OnInit{
     );
   }
 
-  snimiProdavac(editForm:NgForm) {
-    if (editForm.form.valid){
+  snimiProdavac(editForm: NgForm) {
+    if (editForm.form.valid) {
       this.prodavacSnimiEndpoint.obradi(this.odabraniProdavac!).subscribe({
         next: (x: any) => {
           this.showProdavacEdit = false;
@@ -84,7 +93,9 @@ export class ProdavacComponent implements OnInit{
           this.fetchProdavac();
           this.showProdavacTable = true;
         },
-        error: (x) => { alert("greska snimiProdavac -> " + x.error) },
+        error: (x) => {
+          alert('greska snimiProdavac -> ' + x.error);
+        },
       });
     }
   }
@@ -103,15 +114,17 @@ export class ProdavacComponent implements OnInit{
     this.showProdavacForm = false;
   }
 
-  brisiProdavac(id:number) {
-    if (confirm("Da li zelite izbrisati Prodavaca"))
+  brisiProdavac(id: number) {
+    if (confirm('Da li zelite izbrisati Prodavaca'))
       this.prodavacBrisiEndpoint.obradi(id).subscribe({
-      next: (x) => {
-        this.fetchProdavac();
-        this.showProdavacTable = true;
-      },
-        error: (x) => { alert("greska brisiProdavac -> " + x.error) },
-    });
+        next: (x) => {
+          this.fetchProdavac();
+          this.showProdavacTable = true;
+        },
+        error: (x) => {
+          alert('greska brisiProdavac -> ' + x.error);
+        },
+      });
   }
 
   dodajNovi() {
@@ -120,19 +133,20 @@ export class ProdavacComponent implements OnInit{
     this.showProdavacTable = false;
     this.showProdavacEdit = false;
     this.noviProdavac = {
-      id:0,
-      ime:'',
-      prezime:'',
-      gradID:1,
-      spolID:1,
-      isProdavac:true,
-      username:'',
-      email:'',
-    }
+      id: 0,
+      ime: '',
+      prezime: '',
+      gradID: 1,
+      spolID: 1,
+      isProdavac: true,
+      username: '',
+      email: '',
+      slikaKorisnikaNovaString: '',
+    };
   }
 
   dodajProdavac(dodajForm: NgForm) {
-    if (dodajForm.form.valid){
+    if (dodajForm.form.valid) {
       this.prodavacSnimiEndpoint.obradi(this.noviProdavac!).subscribe({
         next: (x: any) => {
           this.showProdavacEdit = false;
@@ -140,9 +154,31 @@ export class ProdavacComponent implements OnInit{
           this.showProdavacTable = true;
           this.showProdavacForm = false;
         },
-        error: (x) => { alert("greska dodajProdavac -> " + x.error) },
+        error: (x) => {
+          alert('greska dodajProdavac -> ' + x.error);
+        },
       });
     }
   }
 
+  //to show the image in preview box
+  generisiPreview() {
+    // @ts-ignore
+    let file = document.getElementById('slika-input').files[0];
+    if (file && this.odabraniProdavac) {
+      let reader = new FileReader();
+      reader.onload = () => {
+        this.odabraniProdavac!.slikaKorisnikaNovaString =
+          reader.result?.toString();
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  //to load the image from db and to add the missing part
+  showSlikaFromDB() {
+    return (
+      'data:image/png;base64,' + this.odabraniProdavac!.slikaKorisnikaNovaString
+    );
+  }
 }
--- ITServiceCenter-frontend/src/app/endpoints/prodavac-endpoints/prodavac-get-all-endpoint.ts
+++ ITServiceCenter-frontend/src/app/endpoints/prodavac-endpoints/prodavac-get-all-endpoint.ts
@@ -1,29 +1,32 @@
-import { Observable } from "rxjs";
-import {MyBaseEndpoint} from "../my-base-endpoint";
-import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {ConfigFile} from "../../configFile";
+import { Observable } from 'rxjs';
+import { MyBaseEndpoint } from '../my-base-endpoint';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ConfigFile } from '../../configFile';
 
-@Injectable({providedIn:"root"})
-export class ProdavacGetAllEndpoint implements MyBaseEndpoint<void, ProdavacGetAllResponse> {
-  constructor(private httpKlijent :HttpClient) {}
-    obradi(request: void): Observable<ProdavacGetAllResponse> {
-      let url = ConfigFile.adresa_servera + "/Prodavac/GetAll";
-        return this.httpKlijent.get<ProdavacGetAllResponse>(url);
-    }
+@Injectable({ providedIn: 'root' })
+export class ProdavacGetAllEndpoint
+  implements MyBaseEndpoint<void, ProdavacGetAllResponse>
+{
+  constructor(private httpKlijent: HttpClient) {}
+  obradi(request: void): Observable<ProdavacGetAllResponse> {
+    let url = ConfigFile.adresa_servera + '/Prodavac/GetAll';
+    return this.httpKlijent.get<ProdavacGetAllResponse>(url);
+  }
 }
 
 export interface ProdavacGetAllResponse {
-  listaProdavac : ProdavacGetAllResponseProdavac [] ;
+  listaProdavac: ProdavacGetAllResponseProdavac[];
 }
 
 export interface ProdavacGetAllResponseProdavac {
-  id:number;
-  ime:string;
-  prezime:string;
-  username:string;
-  email:string;
-  gradID:number;
-  spolID:number;
-  isProdavac:boolean;
+  id: number;
+  ime: string;
+  prezime: string;
+  username: string;
+  email: string;
+  gradID: number;
+  spolID: number;
+  isProdavac: boolean;
+  slikaKorisnikaNovaString: string | undefined;
 }
