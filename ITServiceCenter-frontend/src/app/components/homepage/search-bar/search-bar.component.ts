b26eb643ad86149e08ae5c699ba8b9412cd7d7f4
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-04-30 19:10:02 +0200

    update

--- ITServiceCenter-frontend/src/app/components/homepage/search-bar/search-bar.component.ts
+++ ITServiceCenter-frontend/src/app/components/homepage/search-bar/search-bar.component.ts
@@ -1,12 +1,18 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+  styleUrl: './search-bar.component.css',
 })
 export class SearchBarComponent {
-
+  constructor() {}
+  pretregaObj: any = {
+    id: 0,
+    sifra: '',
+  };
 }
